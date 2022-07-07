d0f6e1dd9aee9db92617d9a1a95b9d5b36743ab3
9804737914 <108190654+9804737914@users.noreply.github.com>
2022-07-07 17:59:55 +0545

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+	float area,peri,vol,l=3.0;
+	area=6.0*l*l;
+	peri=12.0*l;
+	vol=l*l*l;
+	printf("%2f is area, /n %2f is perimeter and %2f is volume",area,peri,vol);
+	return 0;
+}
